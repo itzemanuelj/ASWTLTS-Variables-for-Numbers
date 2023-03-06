2d9ea263c1fe7b66a9a9ceead1f977cfb05b941f
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-06 12:24:31 -0500

    Create Variables for Numbers.ts

---
+++ Variables for Numbers.ts
@@ -0,0 +1,26 @@
+/// What is the value of orderTotal?
+
+const merchTotal: number = 100;
+const shippingCharge: number = 10;
+let orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+/// 110
+
+let niceBigNumer: number = 1000000;
+
+/// In a single statement, declare the variable numberAsString and assign the number 3 to it as a string.
+
+let numberAsString: string = "1000";
+
+/// Assign to the variable caseQty, which has already been declared, the value 144.
+
+let castQty: number = 144;
+
+/// In a single statement declare a variable and assign a number to it.
+
+let total: number = 100;
+
+/// In a single statement declare a variable and assign to it the sum of 2 other variables
+
+let;
